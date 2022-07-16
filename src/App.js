517af4bdb2223cdaf7b020e0356a586523681cfd
Anukarsh <anukarshr@gmail.com>
2022-07-16 22:51:29 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
     </div>
   </div>
 </nav>
+<Navbar/>
     </>
   );
 }
