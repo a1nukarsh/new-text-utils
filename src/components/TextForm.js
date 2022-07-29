c0e836d13db5421562e4524a7901d2f9df66957f
Anukarsh <anukarshr@gmail.com>
2022-07-29 13:55:32 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,16 +1,27 @@
 import React, {useState} from 'react'
 
-const[text, setText] = useState('Enter text here');
 
 
 export default function TextForm(props) {
+  const handleUpClick = ()=>{
+    console.log("UPPER CASE WAS CLICKED")
+    let newText = text.toUpperCase()
+    setText(newText)
+  }
+  const handleOnChange = (event)=>{
+    console.log("ON CHANGE")
+    setText(event.target.value)
+  }
+  const[text, setText] = useState('Enter text here2');
+  //text="new"// wrong way toh change state
+  //setText("New Text"); // correct way to change state
   return (
     <div>
     <h1>{props.heading}</h1>
     <div className="mb-3">
-        <textarea className="form-control" id="textArea" rows="8"></textarea>
+        <textarea className="form-control" value= {text} onChange={handleOnChange} id="textArea" rows="8"></textarea>
     </div>
-    <button className="btn btn-primary">Convert to UPPER Case</button>
+    <button className="btn btn-primary" onClick={handleUpClick}>Convert to UPPER Case</button>
  
     </div>
   )
