119ce6047859e0c2a2e1292726b7ffd43c4e14b5
Anukarsh <anukarshr@gmail.com>
2022-08-01 23:56:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-import About from './components/About';
+// import About from './components/About';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
       <Navbar title ="TxtEdtrs" />
       <div className="container my-3">
       <TextForm heading="Enter your text here"/>
-      <About/>
+      {/* <About/> */}
       </div>
     </>
   );
