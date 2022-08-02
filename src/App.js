61ac0838698773c337e3763b4409eb0ece709413
Anukarsh <anukarshr@gmail.com>
2022-08-02 18:16:06 +0530

    navabar dark mode

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 // import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 // import About from './components/About';
 
 function App() {
-  return (
+  const [mode, setMode] = useState('light')
+
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark')
+    }
+    else{
+      setMode('light')
+    }
+  }  
+return (
     <>
-      <Navbar title ="TxtEdtrs" />
+      <Navbar title ="TxtEdtrs" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
       <TextForm heading="Enter your text here"/>
       {/* <About/> */}
